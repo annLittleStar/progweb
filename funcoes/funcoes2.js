70d29605dc7c14b2a1965c7485eb6bfddd665ac4
Gabriel-Barboza <55238889+Gabriel-Barboza@users.noreply.github.com>
2019-09-28 19:59:59 -0300

    Correções de linhas

--- funcoes/funcoes2.js
+++ funcoes/funcoes2.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	/*Colocar as funções para testar, antes de colocar tudo num mesmo arquivo*/
 	$("#confirma").onclick(function(){
 		if(/*indicar condição de email e nome*/) {
-			
+
 		}
 	})
 
