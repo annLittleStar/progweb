d85bdfcb1885b9605b9f017215a17b45cb890bd1
annLittleStar <ana.schran@pucpr.edu.br>
2019-11-16 02:42:52 -0300

    Atualização 10
    php aí olha...
    acho q fiz algo errado mas comecei kkkk
    Yay :3

--- projeto/js/funcoes.js
+++ projeto/js/funcoes.js
@@ -5,3 +5,56 @@ $(document).ready(function(){
 		window.location.href = "paginas/caixaDeEntrada.html"
 	});
 });
+
+$(document).ready(function(){
+	$("#menu2").click(function(){
+
+		var remetente = $("#remetente").val();
+		var assunto = $("#assunto").val();
+		var trecho = $("#trecho").val();
+
+		$.ajax({
+			type: "POST",
+			url: "../php/parte01.php",
+			dataType: "json",
+			data:{
+				ajaxRemetente: remetente,
+				ajaxAssunto: assunto,
+				ajaxTrecho: trecho
+			},
+			success: function(retorno){
+
+			}
+		})
+	})
+
+	$("#menu02").click(function(){
+		$.ajax({
+			type:"POST",
+			dataType: "json",
+			url:"../php/parte02.php",
+		
+		success: function(retorno){
+
+			
+			var conteudo = "";
+			conteudo += "<table>";
+			conteudo += "<tr>";
+			conteudo += "<td>" + retorno.remetente + "</td>";
+			conteudo += "</tr>";
+
+			conteudo += "<tr>";
+			conteudo += "<td>" + retorno.assunto + "</td>";
+			conteudo += "</tr>";
+
+			conteudo += "<tr>";
+			conteudo += "<td>" + retorno.trecho + "</td>";
+			conteudo += "</tr>";
+
+			conteudo += "</table>";
+
+			$("#lista").html(conteudo);
+			}
+		})
+	})
+})
